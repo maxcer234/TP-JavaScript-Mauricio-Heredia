a2cf302a76cbe2ec0ced49df9f88679672b66875
Maurcio Heredia <maurih46322945@gmail.com>
2024-05-03 13:42:42 -0300

    Se termino el cuarto TP de JS

--- Assets/diaSemana.js
+++ Assets/diaSemana.js
@@ -2,5 +2,26 @@ let dia = parseInt(prompt("Ingrese el numero de la Semana"))
 
 switch(dia){
     case 1:
-        console.log("LUNES")
+        console.log("Lunes")
+        break;
+    case 2:
+        console.log("Martes")
+        break;
+    case 3:
+        console.log("Miercole")
+        break;
+    case 4:
+        console.log("Jueves")
+        break;
+    case 5:
+        console.log("Viernes")
+        break;
+    case 6:
+        console.log("Sabado")
+        break;
+    case 7:
+        console.log("Domingo")
+        break;
+    default:
+        console.log("Numero ingresado no coincide con el numero de la semana")
 }
