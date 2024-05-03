b7c055dea554513e6b7bc31ff4303948742e2d71
Maurcio Heredia <maurih46322945@gmail.com>
2024-05-03 09:50:39 -0300

    Se finalizo el primer TP de JS

--- Assets/RangoEdad.js
+++ Assets/RangoEdad.js
@@ -1,1 +1,14 @@
 let edad = prompt("Ingrese su edad");
+
+if( edad>0 && edad<=12){
+    console.log("Es niño");
+}
+else if ( edad >= 13 && edad<19){
+    console.log("Es adolecente")
+}
+else if (edad >=20 && edad<=60){
+    console.log ("Es Adulto")
+}
+else if (edad >= 60){
+    console.log("Es Adulto Mayor")
+}
