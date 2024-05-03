f413067fe39ca6b41f714b86f0367d458f32bd85
Maurcio Heredia <maurih46322945@gmail.com>
2024-05-03 10:03:38 -0300

    Se finalizo el segundo TP de JS

--- Assets/parlmpar.js
+++ Assets/parlmpar.js
@@ -1,2 +1,7 @@
-let numero = prompt("Ingrese un numero")
+let numero = parseInt(prompt("Ingrese un numero"));
 
+if (numero % 2 == 0){
+    console.log ( numero + " Es numero par")
+} else {
+    console.log( numero + " Es numero inpar")
+}
